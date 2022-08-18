744c126ffd4baa6b61b77fb8769bac31b869eb7d
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-18 13:05:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const findMatching = (drivers, str) => {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === str.toLowerCase();
+    })
+}
+
+const fuzzyMatch = (drivers, str) => {
+    const strLength = str.length;
+    return drivers.filter((driver) => {
+        return driver.slice(0,strLength) === str;
+    })
+}
+
+const matchName = (drivers, str) => {
+    const result = drivers.filter((driver) => {
+        return driver.name === str;
+    })
+
+    return result;
+}
